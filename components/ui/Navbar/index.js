6a3386568ec4e4e843510de99a002b8cc7213064
ManPeach0x <TraitForge@protonmail.com>
2024-05-03 13:30:26 +0930

    home paragrraph

--- components/ui/Navbar/index.js
+++ components/ui/Navbar/index.js
@@ -1,6 +1,10 @@
 import Link from 'next/link';
-import { Logo } from '@/icons';
+import styles from './styles.module.scss';
+import { Logo } from '@/icons'
 import { useRouter } from 'next/router';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDiscord, faTwitter, faTelegram  } from '@fortawesome/free-brands-svg-icons';
 
 const links = [
     { url: '/', text: 'HOME' },
@@ -10,15 +14,24 @@ const links = [
     { url: 'https://www.traitforge.game/', text: 'PLAY NOW' },
   ];
 
+  const mediaLinks = [
+    { url: 'https://t.me/+b31jKqlV-1BjNzY1', icon: faTelegram},
+    { url: 'https://twitter.com/TraitForge', icon: faTwitter},
+    { url: 'https://discord.gg/KWHCEY6zFT', icon: faDiscord},
+  ]
+
 const Navbar = () => {
     const router = useRouter();
 
     return (
-        <nav className="navbar bg-gray-800 text-white p-4">
-            <Link href="/" className="inline-block">
+        <nav className="navbar text-white p-2">
+            <Link href="/">
               <Logo />
             </Link>
-            <div className="container mx-auto flex justify-end">
+          <div className="container absolute right-5 top-6 flex justify-end">
+
+            <div className="container flex justify-end mr-10 mt-1.5">
+            <ul className="flex space-x-4 text-xl list-none">
             {links.map((link, index) => (
             <li key={index}>
               <Link
@@ -29,7 +42,24 @@ const Navbar = () => {
               </Link>
             </li>
           ))}
+          </ul>
             </div>
+
+            <div className="ml-2">
+            <ul className="flex space-x-4 text-3xl list-none">
+            {mediaLinks.map((link, index) => (
+            <li key={index}>
+              <Link
+                className={`${link.url === router.asPath}`}
+                href={link.url}
+              >
+            <FontAwesomeIcon icon={link.icon}/>
+              </Link>
+            </li>
+          ))}
+           </ul>
+            </div>
+          </div>
         </nav>
     );
 };
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,15 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="pages">
-        <h1 className="text-5xl my-6"> TraitForge </h1>
+        <h1 className="text-6xl mt-9 mb-6"> About TraitForge </h1>
+        <p className="w-6/12 text-3xl text-center mb-10 border-b-2"> 
+          TraitForge, launched in 2024, is at the forefront of gaming innovation, 
+          utilizing blockchain technology to ReInvent GameFi. The company is committed to developing unique, 
+          immersive games that leverage the benefits of DeFi.
+        </p>
+        
+        <h2> 
+        </h2>
     </div>
 )
 };
