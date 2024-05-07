9a0562015159380410c66c36fb696bdbe0065e18
ManPeach0x <TraitForge@protonmail.com>
2024-05-07 22:03:33 +0930

    footer

--- components/ui/Footer/index.js
+++ components/ui/Footer/index.js
@@ -1,4 +1,4 @@
-import { mediaLinks } from '@/lib/links'; 
+import { mediaLinks, links } from '@/lib/links'; 
 import { useRouter } from 'next/router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Logo } from '@/icons';
@@ -6,7 +6,7 @@ import Link from 'next/link';
  const Footer = () => {
     const router = useRouter();
     return (
-        <footer className="footer text-white text-center p-4 w-full">
+        <footer className="footer text-white flex flex-row text-center p-4 w-full">
             <div className="flex flex-col w-3/12 items-center gap-10 justify-center">
             <Logo/>
              <div>
@@ -25,6 +25,33 @@ import Link from 'next/link';
             <div className="container mx-auto">
                 <p>© {new Date().getFullYear()} TraitForge Inc.</p>
             </div>
+            </div>
+
+        <div className="w-4/12 absolute right-10 gap-36 flex flex-row">
+            <div className="flex flex-col justify-center">
+                <h1 className="mb-2 text-lg"> TRAITFORGE </h1>
+                <ul className="flex gap-y-1 text-sm list-none text-center justify-center flex-col">
+                    {links.map((link, index) => (
+                      <li key={index}>
+                      {link.external ? (
+                      <a href={link.url} target="_blank" rel="noopener noreferrer">
+                       {link.text}
+                      </a>
+                ) : (
+                    <a href={link.url}>{link.text}</a>
+                    )}
+                    </li>
+                ))}
+                </ul>
+             </div>
+
+            <div className="flex flex-col">
+                <h1 className="mb-2 text-lg">ABOUT</h1>  
+                <h1 className="mb-2 text-sm">Contact Us</h1>  
+                <h1 className="mb-2 text-sm">FAQ</h1>  
+                <h1 className="mb-2 text-sm">Privacy Policy</h1>  
+                <h1 className="mb-2 text-sm">Terms of Service</h1>  
+             </div>
             </div>
         </footer>
     );
