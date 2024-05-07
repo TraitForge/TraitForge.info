8d52082fcd26ce8d807f0ba186c745940a066272
ManPeach0x <TraitForge@protonmail.com>
2024-05-07 13:49:29 +0930

    commit

--- pages/gameplaypage.js
+++ pages/gameplaypage.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import styles from '../styles/gameplay.module.scss'; 
+import styles from '../styles/gameplay.module.scss';
 
 const GameplayPage = () => {
   return (
-    <div className='h-screen w-screen flex flex-col items-center'>
+    <div className='h-screen w-screen flex flex-col items-center' class={styles.page}>
       <h1 className="headers text-6xl" title="GAME-PLAY">GAME-PLAY</h1>
       
-      <div className={styles.mintContainer}>
-        <h1 className="text-5xl my-6"> MINTING </h1>
-        <div className="text-lg">
-          10,000 “Gen 1” entities are available to mint. 
-          The first starts at 0.01 ETH and each subsequent one rises linearly by 0.01 ETH until the final is 100 ETH.
-          In total if all are minted then 500,050 ETH is raised per generation.
-          There are a total of 10 Generations, which can be minted in parallel via a breeding mechanism. 
-          In theory, 5m ETH could accrue to the central honeypot. 
-        </div>
-      </div>
+      <div></div>
+      <object type="image/svg+xml" data="/images/frame1.svg" width="1000" height="1000">              
+      <svg width="400" height="200" xmlns="http://www.w3.org/2000/svg">
+         <rect x="0" y="0" width="400" height="200" fill="#FFF"/>
+         <text x="50" y="50" fontSize="24" fill="#000"> MINTING </text>
+         <text x="50" y="80" fontSize="16" fill="#000">10,000 “Gen 1” entities are available to mint.</text>
+         <text x="50" y="100" fontSize="16" fill="#000">The first starts at 0.01 ETH and each subsequent one rises linearly by 0.01 ETH until the final is 100 ETH.</text>
+         <text x="50" y="120" fontSize="16" fill="#000">In total if all are minted then 500,050 ETH is raised per generation.</text>
+         <text x="50" y="140" fontSize="16" fill="#000">There are a total of 10 Generations, which can be minted in parallel via a breeding mechanism.</text>
+         <text x="50" y="160" fontSize="16" fill="#000">In theory, 5m ETH could accrue to the central honeypot.</text>
+      </svg>
+      </object>
 
-      <div className={styles.forgingContainer}>
         <h1 className="text-5xl my-6"> FORGING </h1>
         <div className="text-lg">
           When a Merger merges with a Forger, their entropy is averaged to produce the next generation entity. 
@@ -25,17 +26,13 @@ const GameplayPage = () => {
           which would be halfway between the parameters of the parents. 
           If merging runs out, then the next generation is minted with the genesis entropy off-by-one.
         </div>
-      </div>
 
-      <div className={styles.tradingContainer}>
         <h1 className="text-5xl my-6"> TRADING </h1>
         <div className="text-lg">
           Entities can be traded via the 'Trading' page or traded directly through a marketplace like OpenSea. 
           10% of the trading fee will be routed to the honeypot for extra game economy.
         </div>
-      </div>
 
-      <div className={styles.nukingContainer}>
         <h1 className="text-5xl my-6"> NUKING </h1>
         <div className="text-lg">
           The Nuke Fund accumulates ETH from new mints and economic activity. 
@@ -45,7 +42,7 @@ const GameplayPage = () => {
           Entities move from their initial Nuke Factor (set randomly) towards the maximum with a speed dictated by Performance Factor (set randomly). 
           Some mature faster than others, with the fastest maturing in around 30 days, and the slowest 600 days. 
         </div>
-      </div>
+
     </div>
   );
 }
