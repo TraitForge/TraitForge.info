2ee2f1d90c4054558bbb2d4494877daf0fb15a19
TForge1 <traitforge@protonmail.com>
2024-12-09 16:35:30 +0930

    metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,16 @@ const monserat = Montserrat({
   variable: "--font-monserat",
 });
 
+export const metadata = {
+  title: 'TraitForge | Mint, Nuke, and Forge Unique Entities',
+  description: 'Unleash your creativity with TraitForge. Mint, Nuke, and Forge unique NFTs with distinct traits and endless possibilities.',
+  openGraph: {
+    title: 'TraitForge: Mint, Nuke, and Forge Unique Entities',
+    description: 'Dive into TraitForge, where creativity meets strategy. Mint up to 100,000 distinct NFTs with unique traits. Play, forge, and build your collection!',
+    url: 'https://traitforge.info',
+  },
+};
+
 const App = ({ Component, pageProps }) => (
   <div className={`${monserat.variable} ${myFont.className}`}>
     <Wrapper>
